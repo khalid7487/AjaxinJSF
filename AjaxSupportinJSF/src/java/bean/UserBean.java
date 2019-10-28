de4ca0f630283a60b8a6d59dd2dc788e6062811a
khalid7487 <khalid15-7487@diu.edu.bd>
2019-10-28 13:24:56 +0600

    show value into combobox from db

--- AjaxSupportinJSF/src/java/bean/UserBean.java
+++ AjaxSupportinJSF/src/java/bean/UserBean.java
@@ -13,87 +13,94 @@ import javax.faces.model.SelectItem;
 @ManagedBean
 @SessionScoped
 public class UserBean {
-    private String first_name,last_name,email,uname,pass,regdate,selectedname,msg="";
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
+ 
+    private String first_name, last_name, email, uname, pass, regdate, selectedname, msg="";
+ 
     public String getEmail() {
         return email;
     }
-
+ 
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getUname() {
-        return uname;
+ 
+    public String getFirst_name() {
+        return first_name;
     }
-
-    public void setUname(String uname) {
-        this.uname = uname;
+ 
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
     }
-
+ 
+    public String getLast_name() {
+        return last_name;
+    }
+ 
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+ 
     public String getPass() {
         return pass;
     }
-
+ 
     public void setPass(String pass) {
         this.pass = pass;
     }
-
+ 
     public String getRegdate() {
         return regdate;
     }
-
+ 
     public void setRegdate(String regdate) {
         this.regdate = regdate;
     }
-
+ 
     public String getSelectedname() {
         return selectedname;
     }
-
+ 
     public void setSelectedname(String selectedname) {
         this.selectedname = selectedname;
     }
-
+ 
+    public String getUname() {
+        return uname;
+    }
+ 
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+ 
     public String getMsg() {
         return msg;
     }
-
+ 
     public void setMsg(String msg) {
         this.msg = msg;
     }
-    public List<SelectItem> getFullName(){
-        List<SelectItem> retVal=new ArrayList<SelectItem>();
+ 
+    public List<SelectItem> getFullName() {
+        List<SelectItem> retVal = new ArrayList<SelectItem>();
+ 
         try {
-            Connection con=Database.getConnection();
-            Statement st=con.createStatement();
-            ResultSet rs=null;
-            String myQuery=" select concat(first_name,' ',last_name) as name from members";
-            rs=st.executeQuery(myQuery);
-            while(rs.next()){
+            Connection con = Database.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = null;
+            String myQuery = "SELECT concat(first_name, ' ', last_name) as name FROM members";
+ 
+            rs = st.executeQuery(myQuery);
+            while (rs.next()) {
                 retVal.add(new SelectItem(rs.getString("name")));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+ 
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+ 
         return retVal;
     }
-    
+ 
+   
     
 }
